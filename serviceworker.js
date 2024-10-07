0fef10d23f18b9c508bc3cc419fb468abd6842f9
Pham Tuan Hung <L20220047@SSPTAJI.CZ>
2024-10-07 11:52:52 +0200

    serviceworker

--- serviceworker.js
+++ serviceworker.js
@@ -1,10 +1,36 @@
-self.addEventListener("install", event =>{
-
-    console.log("service worker installed");
-
-});
-self.addEventListener("activate", event => {
-
-    console.log("Service worker activated");
-
-});
+// Registrace service workeru
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then(registration => {
+    console.log('Service Worker registrován s následujícím scope:', registration.scope);
+    }, err => {
+    console.log('Service Worker registrace selhala:', err);
+    });
+    });
+    }
+    // Instalace service workeru
+    self.addEventListener('install', event => {
+    console.log('Service Worker se instaluje...');
+    event.waitUntil(
+    caches.open('my-cache').then(cache => {
+    return cache.addAll([
+    '/',
+    '/index.html',
+    '/style.css',
+    '/script.js'
+    ]);
+    })
+    );
+    });
+    // Aktivace service workeru
+    self.addEventListener('activate', event => {
+    console.log('Service Worker byl aktivov´an');
+    });
+    // Zachyt´av´an´ı poˇzadavk˚u
+    self.addEventListener('fetch', event => {
+    event.respondWith(
+    caches.match(event.request).then(response => {
+    return response || fetch(event.request);
+    })
+    );
+    });
